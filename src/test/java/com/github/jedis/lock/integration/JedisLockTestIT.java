dd1bfd1e247b9d516956b4378670b126835c769a
Bruno Bossola <bbossola@gmail.com>
2015-07-15 14:56:47 +0100

    New integration test that works using system properties

--- src/test/java/com/github/jedis/lock/integration/JedisLockTestIT.java
+++ src/test/java/com/github/jedis/lock/integration/JedisLockTestIT.java
@@ -3,107 +3,157 @@ package com.github.jedis.lock.integration;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Protocol;
 
 import com.github.jedis.lock.JedisLock;
-import com.github.jedis.lock.integration.HostAndPortUtil.HostAndPort;
 
 /**
  * Integration test - please see HostAndPortUtil to set where your server is
- *
- * To launch of course:
- * mvn failsafe:integration-test
- *
+ * 
+ * To launch of course: mvn failsafe:integration-test  passing the necessary JVM arguments, i.e.: 
+ * mvn -Djedis.host=localhost -Djedis.auth=foobared failsafe:integration-test  
+ * 
  */
 public class JedisLockTestIT {
 
-	private static HostAndPort redis1 = HostAndPortUtil.getRedisServers().get(0);
+    private static final String JEDIS_HOST = System.getProperty("jedis.host", "localhost");
+    private static final Integer JEDIS_PORT = Integer.getInteger("jedis.port", Protocol.DEFAULT_PORT);
+    private static final String JEDIS_AUTH = System.getProperty("jedis.auth");
 
-	@Test
-	public void testAcquire() throws InterruptedException {
-		Jedis jedis = new Jedis(redis1.host, redis1.port);
+    @BeforeClass
+    public static void setup() {
+
+        String hostinfo = String.format("endpoint=%s:%d, auth=%s", JEDIS_HOST, JEDIS_PORT, JEDIS_AUTH);
+
+        System.out.println("Using redis at " + hostinfo);
+        Jedis jedis = new Jedis(JEDIS_HOST, JEDIS_PORT);
+        try {
+            connect(jedis);
+        } catch (Exception ex) {
+            System.err.println("Unable to connect to Jedis - " + hostinfo);
+            fail("Unable to connect to Jedis - " + hostinfo);
+        } finally {
+            disconnect(jedis);
+        }
+    }
+
+    @Test
+    public void testAcquire() throws InterruptedException {
+        Jedis jedis = connect();
+
+        JedisLock lock = new JedisLock(jedis, "testlock2");
+        assertTrue(lock.acquire());
+
+        JedisLock lock2 = new JedisLock(jedis, "testlock2", 1000);
+        assertFalse(lock2.acquire());
+
+        lock.release();
+
+        lock2 = new JedisLock(jedis, "testlock2", 1000);
+        assertTrue(lock2.acquire());
+        lock2.release();
+    }
+
+    @Test
+    public void testRenew() throws InterruptedException {
+        Jedis jedis = connect();
+
+        JedisLock lock = new JedisLock(jedis, "testlock2");
+        assertTrue(lock.acquire());
+
+        Thread.sleep(2000l);
+
+        assertTrue(lock.renew());
+
+        lock.release();
+
+        JedisLock lock2 = new JedisLock(jedis, "testlock2", 1000);
+        assertTrue(lock2.acquire());
+        lock2.release();
+    }
+
+    @Test
+    public void testConcurrency() throws InterruptedException {
+        final int count = 10;
+        
+        ConcurrentLocker[] lockers = new ConcurrentLocker[]{
+                new ConcurrentLocker(count), 
+                new ConcurrentLocker(count), 
+                new ConcurrentLocker(count)};
+
+        for (ConcurrentLocker locker : lockers) {
+            locker.start();
+        }
+
+        for (ConcurrentLocker locker : lockers) {
+            locker.join();
+        }
+
+        for (ConcurrentLocker locker : lockers) {
+            assertEquals(count, locker.count());
+        }
+    }
+
+    private class ConcurrentLocker extends Thread {
+
+        private final int times;
+        private int counter;
+        
+        public ConcurrentLocker(int times) {
+            this.times = times;
+            this.counter = 0;
+        }
+        
+        public void run() {
+            Jedis jedis = connect();
+            try {
+                for (int i = 0; i < times; i++) {
+                    JedisLock lock = new JedisLock(jedis, "testlock", 15000, 200);
+                    try {
+                        if (lock.acquire()) {
+                            counter++;
+                            Thread.sleep(250);
+                            lock.release();
+                        }
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        return;
+                    }
+                }
+            } finally {
+                disconnect(jedis);
+            }
+        }
+        
+        public int count() {
+            return counter;
+        }
+    };
+
+    
+    private Jedis connect() {
+        Jedis jedis = new Jedis(JEDIS_HOST, JEDIS_PORT);
+        connect(jedis);
+        return jedis;
+    }
+
+    private static void connect(Jedis jedis) {
         jedis.connect();
-        jedis.auth("foobared");
+        jedis.auth(JEDIS_AUTH);
+    }
 
-		JedisLock lock = new JedisLock(jedis, "testlock2");
-		assertTrue(lock.acquire());
+    private static void disconnect(Jedis jedis) {
+        try {
+            jedis.disconnect();
+        } catch (Throwable ignore) {
+        }
+    }
 
-		JedisLock lock2 = new JedisLock(jedis, "testlock2", 1000);
-		assertFalse(lock2.acquire());
-
-		lock.release();
-
-		lock2 = new JedisLock(jedis, "testlock2", 1000);
-		assertTrue(lock2.acquire());
-
-		lock2.release();
-	}
-
-	@Test
-	public void testConcurrency() throws InterruptedException {
-		final AtomicInteger t0Acquired = new AtomicInteger();
-		final AtomicInteger t1Acquired = new AtomicInteger();
-
-		final int count = 10;
-		
-		final Thread t0 = new Thread(new Runnable() {
-
-			public void run() {
-				Jedis jedis = new Jedis(redis1.host, redis1.port);
-		        jedis.connect();
-		        jedis.auth("foobared");
-
-				for (int i = 0; i < count; i++) {
-					JedisLock lock = new JedisLock(jedis, "testlock", 15000, 200);
-					try {
-						if (lock.acquire()) {
-							t0Acquired.incrementAndGet();
-							Thread.sleep(250);
-							lock.release();
-						}
-						Thread.sleep(100);
-					} catch (InterruptedException e) {
-						return;
-					}
-				}
-			}
-		});
-
-		Thread t1 = new Thread(new Runnable() {
-
-			public void run() {
-				Jedis jedis = new Jedis(redis1.host, redis1.port);
-		        jedis.connect();
-		        jedis.auth("foobared");
-
-				for (int i = 0; i < count; i++) {
-					JedisLock lock = new JedisLock(jedis, "testlock", 15000, 200);
-					try {
-						if (lock.acquire()) {
-							t1Acquired.incrementAndGet();
-							Thread.sleep(100);
-							lock.release();
-						}
-						Thread.sleep(100);
-					} catch (InterruptedException e) {
-						return;
-					}
-				}
-			}
-		});
-
-		t0.start();
-		t1.start();
-
-		t0.join();
-		t1.join();
-
-		assertEquals(count, t0Acquired.get());
-		assertEquals(count, t1Acquired.get());
-	}
 }
